469f43a25995a3d58239bce1aa47549f857ae912
jsamaze <111722374+jsamaze@users.noreply.github.com>
2023-10-18 16:16:40 +0800

    added github pages cors

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const server = http.createServer(app)  //HEROKU
 //integrate socket.io
 export const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL, /\.github\.io$/],
+    origin: [process.env.FRONTEND_URL, "https://jsamaze.github.io"],
     credentials:true,
       
   }, connectionStateRecovery: {
