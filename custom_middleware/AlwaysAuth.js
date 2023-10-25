5c57991e80e9aad81822b6c20e962ee9447583b3
jsamaze <111722374+jsamaze@users.noreply.github.com>
2023-10-25 19:20:54 +0800

    wrong code

--- custom_middleware/AlwaysAuth.js
+++ custom_middleware/AlwaysAuth.js
@@ -1,6 +1,6 @@
 export default function  (req, res, next) {
     if (req.session.user_id) {
-        res.status(301).send({
+        res.status(401).send({
             status : "You are already authenticated",
             username : req.session.username,
             userId : req.session.user_id
